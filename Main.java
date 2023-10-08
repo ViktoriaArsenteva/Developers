43c45a4c05dafda6f83aa0fd9b5c27c41814b352
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2023-10-08 23:10:42 +0300

    Create devs list

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
     
-        FullStackDeveloper FSDev = new FullStackDeveloper();
-        BackendDeveloper BackDev = new BackendDeveloper();
+        // FullStackDeveloper FSDev = new FullStackDeveloper();
+        // BackendDeveloper BackDev = new BackendDeveloper();
         
-        FSDev.CreateFront();
-        FSDev.CreateServer();
-        BackDev.CreateServer();
+        // FSDev.CreateFront();
+        // FSDev.CreateServer();
+        // BackDev.CreateServer();
 
+        ArrayList<Developer> devs = new ArrayList<Developer>();
+        devs.add(new FrontendDeveloper());
+        devs.add(new BackendDeveloper());
+        devs.add(new FullStackDeveloper());
 
-        Developer dev = new FrontendDeveloper();
-        if (dev instanceof Frontender) {
-            ((Frontender) dev).CreateFront();
+        for (Developer dev : devs) {
+            if (dev instanceof FullStack) {
+                ((FullStack) dev).CreateFront();
+                ((FullStack) dev).CreateServer();
+            } else if (dev instanceof Backender) {
+                ((Backender) dev).CreateServer();
+            } else if  (dev instanceof Frontender) {
+                ((Frontender) dev).CreateFront();
+                
+            }
+            
         }
 
         
